61dd9a84592dbd08ab3cabb83b15ff1a9c0ffe2f
Mac3495 <elmac395@gmail.com>
2025-03-20 15:09:52 -0400

    Frontend project init

---
+++ frontend/src/main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+import Login from "./pages/login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
